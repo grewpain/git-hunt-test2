b4d24a1b60e0e9ef8b83b860f5d48b9598d31a07
grewpain <40842469+grewpain@users.noreply.github.com>
2019-03-19 14:31:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
 AWS_ACCESS_KEY_ID="AKIAJ6HTRGFBIGI5ETT4"
 SECRET="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"
+
+from datetime import datetime
+
+now = datetime.now()
+
+mm = str(now.month)
+
+dd = str(now.day)
+
+yyyy = str(now.year)
+
+hour = str(now.hour)
+
+mi = str(now.minute)
+
+ss = str(now.second)
+
+print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+
+username = trendphil\dont_hack_me_please
+password = d0nTh4cKmeE
